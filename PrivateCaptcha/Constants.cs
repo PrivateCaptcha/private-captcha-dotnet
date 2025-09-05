189fc519f42963324fd8a304fce8efe657017ecc
Taras Kushnir <tk.dev@mailbox.org>
2025-09-05 09:40:44 +0300

    Bump version in constants

--- PrivateCaptcha/Constants.cs
+++ PrivateCaptcha/Constants.cs
@@ -3,7 +3,7 @@ namespace PrivateCaptcha;
 internal static class Constants
 {
     public const string DefaultFormField = "private-captcha-solution";
-    public const string Version = "0.0.1";
+    public const string Version = "0.0.2";
     public const string UserAgent = "private-captcha-dotnet/" + Version;
     public const int MinBackoffMillis = 500;
 
